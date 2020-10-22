f09f65b851806042ef717383dd3647c66bd569ca
wellingtonMarinho13 <wellington.gnr@hotmail.com>
2020-10-22 18:09:25 -0300

    correção bug rotas

--- eventex/urls.py
+++ eventex/urls.py
@@ -6,6 +6,8 @@ urlpatterns = [
     # Examples:
     # url(r'^$', 'eventex.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
-    url(r'', home),
+    url(r'^$', home),
     url(r'^admin/', admin.site.urls),
 ]
+
+
