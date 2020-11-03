9b9d32736d6e1b17ec1f0ed7952a238fe833a53b
WellingtonMarinho <wellington.gnr@hotmail.com>
2020-11-03 20:52:14 -0300

    escrevendo testes do formulário

--- eventex/subscriptions/views.py
+++ eventex/subscriptions/views.py
@@ -1,7 +1,18 @@
+from django.core import mail
+from django.http import HttpResponseRedirect
 from django.shortcuts import render
 from eventex.subscriptions.forms import SubscriptionForm
 
 
 def subscribe(request):
-    context = {'form': SubscriptionForm()}
-    return render(request, 'subscriptions/subscription_form.html', context)
+    if request.method == "POST":
+
+        mail.send_mail('Confirmação de inscrição',
+                       'Message',
+                       'contato@eventex.com.br',
+                       ['contato@eventex.com.br', 'umcorte@gmail.com'])
+        return HttpResponseRedirect('/inscricao/')
+
+    else:
+        context = {'form': SubscriptionForm()}
+        return render(request, 'subscriptions/subscription_form.html', context)
