b9e8044cc304709f889355b454635f70618b266f
WellingtonMarinho <wellington.gnr@hotmail.com>
2020-12-07 22:44:14 -0300

    update requirements and settings

--- eventex/settings.py
+++ eventex/settings.py
@@ -40,8 +40,11 @@ INSTALLED_APPS = (
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'django_extensions',
+
     'eventex.core',
     'eventex.subscriptions',
+
 )
 
 MIDDLEWARE = (
