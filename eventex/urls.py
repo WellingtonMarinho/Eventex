4aa09f2d646d05deca13f523dfb5b1a7a5444d41
WellingtonMarinho <wellington.gnr@hotmail.com>
2020-12-08 16:14:53 -0300

    thank you page congratulations

--- eventex/urls.py
+++ eventex/urls.py
@@ -3,11 +3,12 @@ from django.contrib import admin
 
 import eventex.core.views
 
-from eventex.subscriptions.views import subscribe
+from eventex.subscriptions.views import subscribe, detail
 
 urlpatterns = [
     path('', eventex.core.views.home),
     path('inscricao/', subscribe),
+    path('inscricao/<int:pk>/', detail),
     path('admin/', admin.site.urls),
 
 ]
