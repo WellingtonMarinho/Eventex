b4192042f5ca34c10f394d977d8b7fcb1e7ce8f6
Wellington <WellingonMarinho>
2021-01-11 23:33:30 -0300

    marcando como pago

--- eventex/subscriptions/admin.py
+++ eventex/subscriptions/admin.py
@@ -5,10 +5,13 @@ from eventex.subscriptions.models import Subscription
 
 
 class SubscriptionModelAdmin(admin.ModelAdmin):
-    list_display = ('name', 'email', 'phone', 'created_at', 'subscribed_today', 'paid')
+    list_display = ('name', 'email', 'phone', 'created_at',
+                    'subscribed_today', 'paid')
     date_hierarchy = 'created_at'
-    search_fields = ('name', 'email', 'phone', 'created_at')
+    search_fields = ('name', 'email', 'phone', 'cpf', 'created_at')
     list_filter = ('paid', 'created_at')
+
+    actions = ['mark_as_paid']
 
     def subscribed_today(self, obj):
         return obj.created_at == now().date()
@@ -16,4 +19,14 @@ class SubscriptionModelAdmin(admin.ModelAdmin):
     subscribed_today.short_description = 'inscrito hoje?'
     subscribed_today.boolean = True
 
+    def mark_as_paid(self, request, queryset):
+        count = queryset.update(paid=True)
+        if count == 1:
+            msg = '{} inscrição foi marcada como paga.'
+        else:
+            msg = '{} inscrições foram marcadas como pagas.'
+
+        self.message_user(request, msg.format(count))
+
+    mark_as_paid.short_description = 'Marcar como pago'
 admin.site.register(Subscription, SubscriptionModelAdmin)
