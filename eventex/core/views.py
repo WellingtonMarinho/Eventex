d98905dc570b7117994b170b12d479649cae7ee3
WellingtonMarinho <wellington.gnr@hotmail.com>
2021-03-16 00:31:10 -0300

    Finalizando speakers

--- eventex/core/views.py
+++ eventex/core/views.py
@@ -3,10 +3,11 @@ from eventex.core.models import Speaker
 
 
 def home(request):
-    speakers = [
-        {'name': 'Grace Hopper', 'photo': 'http://hbn.link/hopper-pic'},
-        {'name': 'Alan Turing', 'photo': 'http://hbn.link/turing-pic'}
-    ]
+    speakers = Speaker.objects.all()
+    # speakers = [
+    #     {'name': 'Grace Hopper', 'photo': 'http://hbn.link/hopper-pic'},
+    #     {'name': 'Alan Turing', 'photo': 'http://hbn.link/turing-pic'}
+    # ]
     return render(request, 'index.html', {'speakers': speakers})
 
 
